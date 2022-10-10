2503d9e89a8551ced7e2a313d4cbdd8f976b9630
Vishal-493 <114832602+Vishal-493@users.noreply.github.com>
2022-10-10 13:37:52 +0100

    Update primes.py

--- primes.py
+++ primes.py
@@ -3,4 +3,17 @@
 
 def primes(number_of_primes):
     list = []
+    newList = []
+    factorCount = 0
+    for i in range(100000000000000000000000000000000):
+        for j in range(100000000000000000000000000000000): 
+            if i%j == 0:
+                factorCount++
+                continue
+    if factorCount == 2:
+        newList.append(i)
+    newList.sort()
+    while x < number_of_primes:
+        list.append(newList[x])
+            
     return list

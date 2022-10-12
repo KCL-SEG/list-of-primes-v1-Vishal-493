611ff462d995de340bfc21f5b1cc5dd3c26290ab
Vishal-493 <114832602+Vishal-493@users.noreply.github.com>
2022-10-12 18:51:20 +0100

    Update primes.py

--- primes.py
+++ primes.py
@@ -1,28 +1,7 @@
 """List of prime numbers generator."""
 """ENTER YOUR SOLUTION HERE!"""
 
-#def primes(number_of_primes):
- #   list = []
-  #  newList = []
-   # a = 0
-    #for i in range(number_of_primes*5):
-     #   z = i + 1
-      #  for j in range(1, z):
-       #     if i == 0 or j == 0:
-        #        pass
-         #   else:
-          #      if i%j == 0:
-           ##         a+=1
-             #       continue
-#        if a == 2:
- #           newList.append(i)
-  #  newList.sort()
-   # x = 0
-  #  while x < number_of_primes:
-   #     list.append(newList[x])
-    #    x += 1
-            
-  #  return list
+
 def primes(number_of_primes):
     list = []
     while 1:
@@ -37,6 +16,8 @@ def primes(number_of_primes):
                 break
             else:
                 continue
-            return list
+        break
+    return list
+       
  
                 
